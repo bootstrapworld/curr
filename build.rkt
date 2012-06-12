cfc352e5714c2ea262c440722e3bab8fa7e05cdc
Danny Yoo <dyoo@cs.wpi.edu>
2012-06-12 11:00:58 -0400

    build.rkt build script

--- build.rkt
+++ build.rkt
@@ -9,6 +9,9 @@
 (define-runtime-path units-dir
   (build-path "courses" "bs1" "units"))
 
+(define-runtime-path lessons-dir
+  (build-path "lessons"))
+
 (define-runtime-path bs1-main
   (build-path "courses" "bs1" "main.scrbl"))
 
@@ -23,7 +26,10 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(for ([subdir (directory-list units-dir)])
+;; Building the bs1 course
+(printf "build.rkt: building bs1\n")
+(for ([subdir (directory-list units-dir)]
+      #:when (directory-exists? (build-path units-dir subdir)))
   (define scribble-file (build-path units-dir subdir "the-unit.scrbl"))
   (cond [(file-exists? scribble-file)
          (printf "build.rkt: Building ~a\n" scribble-file)
@@ -32,4 +38,19 @@
          (printf "Could not find a \"the-unit.scrbl\" in directory ~a\n"
                  (build-path units-dir subdir))]))
 
+
+;; Building the lessons
+(printf "build.rkt: building lessons\n")
+(for ([subdir (directory-list lessons-dir)]
+      #:when (directory-exists? (build-path lessons-dir subdir)))
+  (define scribble-file (build-path lessons-dir subdir "lesson" "lesson.scrbl"))
+  (cond [(file-exists? scribble-file)
+         (printf "build.rkt: Building ~a\n" scribble-file)
+         (run-scribble scribble-file)]
+        [else
+         (printf "Could not find a \"the-unit.scrbl\" in directory ~a\n"
+                 (build-path lessons-dir subdir))]))
+
+
+(printf "build.rkt: building bs1 main\n")
 (run-scribble bs1-main)
