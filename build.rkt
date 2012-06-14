180f0c0a3ceffb0839cfa0979f214d6edd8044ab
Danny Yoo <dyoo@cs.wpi.edu>
2012-06-14 14:50:15 -0400

    fixing the worksheets so they're real Racket modules

--- build.rkt
+++ build.rkt
@@ -43,7 +43,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Building the bs1 course
 (printf "build.rkt: building bs1\n")
-(for ([subdir (directory-list units-dir)]
+#;(for ([subdir (directory-list units-dir)]
       #:when (directory-exists? (build-path units-dir subdir)))
   (define scribble-file (build-path units-dir subdir "the-unit.scrbl"))
   (cond [(file-exists? scribble-file)
@@ -54,9 +54,9 @@
                  (build-path units-dir subdir))]))
 
 
-;; Building the lessons and its worksheets
+;; Building the lessons
 (printf "build.rkt: building lessons\n")
-(for ([subdir (directory-list lessons-dir)]
+#;(for ([subdir (directory-list lessons-dir)]
       #:when (directory-exists? (build-path lessons-dir subdir)))
   (define scribble-file (build-path lessons-dir subdir "lesson" "lesson.scrbl"))
   (cond [(file-exists? scribble-file)
@@ -64,12 +64,17 @@
          (run-scribble scribble-file)]
         [else
          (printf "Could not find a \"the-unit.scrbl\" in directory ~a\n"
-                 (build-path lessons-dir subdir))])
+                 (build-path lessons-dir subdir))]))
+
+;; and the worksheets.
+(for ([subdir (directory-list lessons-dir)]
+      #:when (directory-exists? (build-path lessons-dir subdir)))
   (when (directory-exists? (build-path lessons-dir subdir "worksheets"))
     (for ([worksheet (directory-list (build-path lessons-dir subdir "worksheets"))]
           #:when (regexp-match #px".scrbl$" worksheet))
-       (printf "build.rkt: building worksheet ~a\n" worksheet)
+       (printf "build.rkt: building worksheet ~a: ~a\n" subdir worksheet)
        (run-scribble (build-path lessons-dir subdir "worksheets" worksheet)))))
+
 
 
 (printf "build.rkt: building bs1 main\n")
