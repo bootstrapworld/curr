3142c6a43f793b8466dba29718ec2b20808e13e5
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2012-09-01 18:12:47 -0400

    fix teachpack link in flags.rkt

--- courses/bs1/resources/source-files/Flags.rkt
+++ courses/bs1/resources/source-files/Flags.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname Flags) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-(require bootstrap2012/bootstrap-teachpack)
+(require "Teachpacks/bootstrap-teachpack.rkt")
 
 ; 1) start with a red dot, of radius 50
 (define dot (circle 50 "solid" "red"))
