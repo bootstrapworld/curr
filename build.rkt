8949efc737bd7541e114fc1e69727df84e0e847c
Danny Yoo <dyoo@hashcollision.org>
2012-12-07 19:29:04 -0700

    refactoring code to make it easier to do bs2 and bs1 together

--- build.rkt
+++ build.rkt
@@ -54,8 +54,8 @@
                             (error 'run-scribble "No deployment directory?")
                             ;; In-place rendering
                             #;(let-values ([(base name dir?)
-                                          (split-path (simple-form-path scribble-file))])
-                              base)]))
+                            (split-path (simple-form-path scribble-file))])
+                            base)]))
   (define-values (base name dir?) (split-path scribble-file))
   (define output-path (build-path output-dir (string->path (regexp-replace #px"\\.scrbl$" (path->string name) ".html"))))
   (parameterize ([current-directory base]
@@ -81,14 +81,14 @@
    #:program "build"
    #:once-each
    [("--course") -course "Choose course (default bs1)"
-                 (current-course -course)]
+    (current-course -course)]
    [("--worksheet-links-to-pdf") "Direct worksheet links to StudentWorkshop.pdf" 
-                                 (putenv "WORKSHEET-LINKS-TO-PDF" "true")]
+    (putenv "WORKSHEET-LINKS-TO-PDF" "true")]
    
    [("--deploy") -deploy-dir "Deploy into the given directory, and create a .zip.  Default: deploy" 
-                 (current-deployment-dir (simple-form-path -deploy-dir))]
+    (current-deployment-dir (simple-form-path -deploy-dir))]
    [("--pdf") "Generate PDF documentation"
-                 (current-generate-pdf? #t)]
+    (current-generate-pdf? #t)]
    
    #:args tags
    tags))
@@ -96,113 +96,137 @@
 
 
 
-
-(void (putenv "SCRIBBLE_TAGS" (string-join current-contextual-tags " ")))
-(printf "build.rkt: tagging context is: ~s\n" current-contextual-tags)
-(printf "deployment path: ~s\n" (current-deployment-dir))
-(printf "-------\n")
-
+(define (initialize-tagging-environment)
+  (void (putenv "SCRIBBLE_TAGS" (string-join current-contextual-tags " ")))
+  (printf "build.rkt: tagging context is: ~s\n" current-contextual-tags)
+  (printf "deployment path: ~s\n" (current-deployment-dir))
+  (printf "-------\n"))
 
 
 
 ;; Building the units of the course.
 ;; We must do this twice to resolve cross references for lessons.
-(printf "build.rkt: building ~a\n" (current-course))
-(for ([phase (in-range 2)])
-  (printf "Phase ~a\n" phase)
-  (for ([subdir (directory-list (get-units-dir))]
-        #:when (directory-exists? (build-path (get-units-dir) subdir)))
-    (define scribble-file (simple-form-path (build-path (get-units-dir) subdir "the-unit.scrbl")))
-    (cond [(file-exists? scribble-file)
-           (printf "build.rkt: Building ~a\n" scribble-file)
-           (copy-file (build-path "lib" "box.gif") 
-                      (build-path (get-units-dir) subdir "box.gif")
-                      #t)
-           (run-scribble scribble-file #:never-generate-pdf? (= phase 0))]
-          [else
-           (printf "Could not find a \"the-unit.scrbl\" in directory ~a\n"
-                   (build-path (get-units-dir) subdir))])))
+(define (build-course-units course)
+  (printf "build.rkt: building ~a\n" course)
+  (for ([phase (in-range 2)])
+    (printf "Phase ~a\n" phase)
+    (for ([subdir (directory-list (get-units-dir))]
+          #:when (directory-exists? (build-path (get-units-dir) subdir)))
+      (define scribble-file (simple-form-path (build-path (get-units-dir) subdir "the-unit.scrbl")))
+      (cond [(file-exists? scribble-file)
+             (printf "build.rkt: Building ~a\n" scribble-file)
+             (copy-file (build-path "lib" "box.gif") 
+                        (build-path (get-units-dir) subdir "box.gif")
+                        #t)
+             (run-scribble scribble-file #:never-generate-pdf? (= phase 0))]
+            [else
+             (printf "Could not find a \"the-unit.scrbl\" in directory ~a\n"
+                     (build-path (get-units-dir) subdir))])))
+
+
+  (printf "build.rkt: building ~a main\n" course)
+  (run-scribble (get-course-main)))
+
 
 ;; Building the lessons
-(printf "build.rkt: building lessons\n")
-(for ([subdir (directory-list lessons-dir)]
-      #:when (directory-exists? (build-path lessons-dir subdir)))
-  (define scribble-file (simple-form-path (build-path lessons-dir subdir "lesson" "lesson.scrbl")))
-  (cond [(file-exists? scribble-file)
-         (printf "build.rkt: Building ~a\n" scribble-file)
-         (run-scribble scribble-file #:never-generate-pdf? #t)]
+(define (build-lessons)
+  (printf "build.rkt: building lessons\n")
+  (for ([subdir (directory-list lessons-dir)]
+        #:when (directory-exists? (build-path lessons-dir subdir)))
+    (define scribble-file (simple-form-path (build-path lessons-dir subdir "lesson" "lesson.scrbl")))
+    (cond [(file-exists? scribble-file)
+           (printf "build.rkt: Building ~a\n" scribble-file)
+           (run-scribble scribble-file #:never-generate-pdf? #t)]
+          [else
+           (printf "Could not find a \"lesson.scrbl\" in directory ~a\n"
+                   (build-path lessons-dir subdir))]))
+
+  ;; and the long-lessons
+  (printf "build.rkt: building long lessons\n")
+  (for ([subdir (directory-list lessons-dir)]
+        #:when (directory-exists? (build-path lessons-dir subdir)))
+    (define scribble-file (simple-form-path (build-path lessons-dir subdir "lesson" "lesson-long.scrbl")))
+    (cond [(file-exists? scribble-file)
+           (printf "build.rkt: Building ~a\n" scribble-file)
+           (run-scribble scribble-file #:never-generate-pdf? #t)])))
+
+
+(define (build-worksheets)
+  ;; and the worksheets
+  (for ([subdir (directory-list lessons-dir)]
+        #:when (directory-exists? (build-path lessons-dir subdir)))
+    (when (directory-exists? (build-path lessons-dir subdir "worksheets"))
+      (for ([worksheet (directory-list (build-path lessons-dir subdir "worksheets"))]
+            #:when (regexp-match #px".scrbl$" worksheet))
+        (printf "build.rkt: building worksheet ~a: ~a\n" subdir worksheet)
+        (run-scribble (build-path lessons-dir subdir "worksheets" worksheet))))))
+
+
+
+(define (build-drills)
+  ;; and the drills
+  (for ([subdir (directory-list lessons-dir)]
+        #:when (directory-exists? (build-path lessons-dir subdir)))
+    (when (directory-exists? (build-path lessons-dir subdir "drills"))
+      (for ([drill (directory-list (build-path lessons-dir subdir "drills"))]
+            #:when (regexp-match #px".scrbl$" drill))
+        (printf "build.rkt: building drill ~a: ~a\n" subdir drill)
+        (run-scribble (build-path lessons-dir subdir "drills" drill))))))
+
+
+
+(define (build-resources course)
+  ;; Under deployment mode, include the resources.
+  (when (current-deployment-dir)
+    (when (directory-exists? (get-resources))
+      
+      (let ([input-resources-dir (get-resources)]
+            [output-resources-dir
+             (build-path (current-deployment-dir) "courses" course
+                         "resources")])
+        (when (directory-exists? output-resources-dir)
+          (delete-directory/files output-resources-dir))
+        (copy-directory/files input-resources-dir
+                              (simple-form-path
+                               (build-path output-resources-dir))))))
+
+  ;; copy auxiliary files into units within distribution
+  (when (current-deployment-dir)
+    (for ([subdir (directory-list (get-units-dir))])
+      (copy-file (build-path "lib" "box.gif")
+                 (build-path (current-deployment-dir) "courses"
+                             course "units" subdir "box.gif")
+                 #t)))
+
+
+  ;; Subtle: this must come after we potentially touch the output
+  ;; resources subdirectory.
+  (cond [(file-exists? (get-teachers-guide))
+         (printf "build.rkt: building teacher's guide\n")
+         (run-scribble (get-teachers-guide))]
         [else
-         (printf "Could not find a \"lesson.scrbl\" in directory ~a\n"
-                 (build-path lessons-dir subdir))]))
+         (printf "build.rkt: no teacher's guide found; skipping\n")]))
 
-;; and the long-lessons
-(printf "build.rkt: building long lessons\n")
-(for ([subdir (directory-list lessons-dir)]
-      #:when (directory-exists? (build-path lessons-dir subdir)))
-  (define scribble-file (simple-form-path (build-path lessons-dir subdir "lesson" "lesson-long.scrbl")))
-  (cond [(file-exists? scribble-file)
-         (printf "build.rkt: Building ~a\n" scribble-file)
-         (run-scribble scribble-file #:never-generate-pdf? #t)]))
 
-;; and the worksheets
-(for ([subdir (directory-list lessons-dir)]
-      #:when (directory-exists? (build-path lessons-dir subdir)))
-  (when (directory-exists? (build-path lessons-dir subdir "worksheets"))
-    (for ([worksheet (directory-list (build-path lessons-dir subdir "worksheets"))]
-          #:when (regexp-match #px".scrbl$" worksheet))
-      (printf "build.rkt: building worksheet ~a: ~a\n" subdir worksheet)
-      (run-scribble (build-path lessons-dir subdir "worksheets" worksheet)))))
-
-;; and the drills
-(for ([subdir (directory-list lessons-dir)]
-      #:when (directory-exists? (build-path lessons-dir subdir)))
-  (when (directory-exists? (build-path lessons-dir subdir "drills"))
-    (for ([drill (directory-list (build-path lessons-dir subdir "drills"))]
-          #:when (regexp-match #px".scrbl$" drill))
-      (printf "build.rkt: building drill ~a: ~a\n" subdir drill)
-      (run-scribble (build-path lessons-dir subdir "drills" drill)))))
-
-(printf "build.rkt: building ~a main\n" (current-course))
-(run-scribble (get-course-main))
+(define (archive-as-zip)
+  ;;  Finally, zip up the deployment directory
+  (when (current-deployment-dir)
+    (let-values ([(base file dir?) (split-path (current-deployment-dir))])
+      (parameterize ([current-directory base])
+        (define output-file (build-path base (format "~a.zip" (path->string file))))
+        (when (file-exists? output-file)
+          (delete-file output-file))
+        (zip output-file file)))))
 
 
 
-;; Under deployment mode, include the resources.
-(when (current-deployment-dir)
-  (when (directory-exists? (get-resources))
-    
-    (let ([input-resources-dir (get-resources)]
-          [output-resources-dir
-           (build-path (current-deployment-dir) "courses" (current-course)
-                       "resources")])
-      (when (directory-exists? output-resources-dir)
-        (delete-directory/files output-resources-dir))
-      (copy-directory/files input-resources-dir
-                            (simple-form-path
-                             (build-path output-resources-dir))))))
-
-;; copy auxiliary files into units within distribution
-(when (current-deployment-dir)
-  (for ([subdir (directory-list (get-units-dir))])
-    (copy-file (build-path "lib" "box.gif")
-               (build-path (current-deployment-dir) "courses"
-                           (current-course) "units" subdir "box.gif")
-               #t)))
-
-;; Subtle: this must come after we potentially touch the output
-;; resources subdirectory.
-(cond [(file-exists? (get-teachers-guide))
-       (printf "build.rkt: building teacher's guide\n")
-       (run-scribble (get-teachers-guide))]
-      [else
-       (printf "build.rkt: no teacher's guide found; skipping\n")])
-
-
-;;  Finally, zip up the deployment directory
-(when (current-deployment-dir)
-  (let-values ([(base file dir?) (split-path (current-deployment-dir))])
-    (parameterize ([current-directory base])
-      (define output-file (build-path base (format "~a.zip" (path->string file))))
-      (when (file-exists? output-file)
-        (delete-file output-file))
-      (zip output-file file))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(initialize-tagging-environment)
+(build-course-units (current-course))
+(build-lessons)
+(build-worksheets)
+(build-drills)
+(build-resources (current-course))
+(archive-as-zip)
