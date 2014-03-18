0bad322e111f6ac4e98fef23ab84c81a5e96dd00
Kathi Fisler <kfisler@cs.wpi.edu>
2014-03-18 12:07:30 -0400

    finished design-recipe worksheet API

--- lib/form-elements.rkt
+++ lib/form-elements.rkt
@@ -1427,23 +1427,57 @@
 
 (define htmlRarr (cond-element [html (sxml->element 'rarr)] [else "->"]))
 
+;; retrieves info on whether to show named component from given spec
+;; currently, spec is either a boolean or a list of three booleans
+;;   corresponding to the funname, input, or output, respectively
+(define (show-example-component? spec comp)
+  (let [(pos (case comp 
+               [(funname) first]
+               [(input) second]
+               [(output) third]))]
+    (or (and (boolean? spec) spec) 
+        (and (cons? spec) (pos spec)))))
+
 ;; Create a design-recipe exercise
 ;;   funname: string naming the function
 ;;   directions: instructions for the problem 
+;; For each of the "show" arguments other than show-examples, 
+;;    length must match that of the corresponding component unless empty
+;; num-examples says how many examples to generate.  Some of the examples may be specified
+;;   by populating the example-list and configuring show-examples (keep reading)
+;; example-list is a list of lists giving the input and output: e.g., (list (list 5 '(* 5 2)))
+;;   the list contents can be num/string/sexp -- anything that format can convert to a string
+;; show-examples is a list of configurations of which parts of each elt in example-list to show
+;;    - each configuration is either a boolean or a list of three booleans
+;;    - e.g., show-examples of (list #f (list #t #f #t) #t) says
+;;      * don't show any components of the first example in example-list
+;;      * show the function name and output of the second example in example-list
+;;      * show all components of the third example in example-list
+;;    - show-examples cannot be longer than example-list  
+;;    - if show-examples is shorter than example-list, remaining elts in example-list get configuration #f
+;; if num-examples is larger than length of example-list, additional blank exercises are added after the
+;;   contents of example-list
 (define (design-recipe-exercise funname directions
-                                #:domain-list (domain-list '())
-                                #:show-domains (show-domains '())
+                                #:show-funname-contract? (show-funname-contract? #f)
+                                #:domain-list (domain-list '()) ; list of string
+                                #:show-domains (show-domains '()) ; list of bool, same length as domain-list unless empty
                                 #:range (range #f)
                                 #:show-range? (show-range? #f)
                                 #:purpose (purpose #f)
                                 #:show-purpose? (show-purpose? #f)
-                                #:example-list (example-list '())
-                                #:show-examples (show-examples '())
-                                #:param-list (param-list '())
-                                #:show-params (show-params '())
-                                #:body (body #f)
+                                #:num-examples (num-examples 1) ; number of total example spaces to generate
+                                #:example-list (example-list '()) ;concrete examples -- see doc above func for details
+                                #:show-examples (show-examples '()) ; see doc above func for details
+                                #:show-funname-defn? (show-funname-defn? #f)
+                                #:param-list (param-list '()) ; list of string
+                                #:show-params (show-params '()) ; list of bool, same length as param-list unless empty
+                                #:body (body #f) ; a string
                                 #:show-body? (show-body? #f)
                                 )
+  (unless (or (empty? show-domains) (= (length show-domains) (length domain-list)))
+    (error 'design-recipe-exercise 
+           (format "Functon ~a: show-domains ~a must be empty or same length as domain-list ~a ~n" 
+                   funname show-domains domain-list)))
   (nested #:style (bootstrap-div-style/extra-id "segment" "exercises")
           (interleave-parbreaks/all
            (list
@@ -1452,45 +1486,66 @@
             (nested #:style (bootstrap-div-style "designRecipeLayout")
                     (interleave-parbreaks/all
                      (list
-                      (design-recipe-section "recipe_contract" 
-                                             "Contract and Purpose Statement"
-                                             "Every contract has three parts ..."
-                                             (make-spacer ";")
-                                             (make-wrapper
-                                              (dr-student-answer #:id? #f "recipe_name" funname)
-                                              (para #:style (bootstrap-span-style "") ":")
-                                              (dr-student-answer "recipe_domain" domain-list)
-                                              (para #:style (bootstrap-span-style "") htmlRarr)
-                                              (dr-student-answer "recipe_range" range))
-                                             (make-clear)
-                                             (make-spacer ";")
-                                             (make-wrapper
-                                              (dr-student-answer "recipe_purpose" purpose)))
+                      (design-recipe-section
+                       "recipe_contract" 
+                       "Contract and Purpose Statement"
+                       "Every contract has three parts ..."
+                       (make-spacer ";")
+                       (make-wrapper
+                        (dr-student-answer #:id? #f "recipe_name" #:show? show-funname-contract? funname)
+                        (para #:style (bootstrap-span-style "") ":")
+                        (dr-student-answer "recipe_domain" #:show? (cons? show-domains) 
+                                           (string-join domain-list " "))
+                        (para #:style (bootstrap-span-style "") htmlRarr)
+                        (dr-student-answer "recipe_range" #:show? show-range? range))
+                       (make-clear)
+                       (make-spacer ";")
+                       (make-wrapper
+                        (dr-student-answer "recipe_purpose" #:show? show-purpose? purpose)))
                       ;; need one of these for each example provided/expected
-                      (design-recipe-section "recipe_examples"
-                                             "Examples"
-                                             "Write some examples of your function in action..."
-                                             (make-spacer "(EXAMPLE ")
-                                             (make-spacer "(")
-                                             (make-wrapper
-                                              (dr-student-answer #:id? #f "recipe_name" funname)
-                                              (dr-student-answer #:id? #f "recipe_example_inputs" "")
-                                              (make-spacer ")")
-                                              ;(make-clear) ; only force this for long-form DR (maybe via a flag?)
-                                              (dr-student-answer #:id? #f "recipe_example_body" "")
-                                              (make-spacer ")")))
-                      (design-recipe-section "recipe_definition"
-                                             "Definition"
-                                             "Write the definition, giving variable names to all your input values..."
-                                             (make-spacer "(define ")
-                                             (make-spacer "(")
-                                             (make-wrapper
-                                              (dr-student-answer #:id? #f "recipe_name" funname)
-                                              (dr-student-answer "recipe_variables" param-list)
-                                              (make-spacer ")")
-                                              ;(make-clear)  ; only force this for long-form DR (maybe via a flag?)
-                                              (dr-student-answer "recipe_definition_body" body)
-                                              (make-spacer ")")))
+                      (design-recipe-section 
+                        "recipe_examples"
+                        "Examples"
+                        "Write some examples of your function in action..."
+                        (let ([example-elts 
+                               (let ([num-blank-examples (- num-examples (length example-list))])
+                                 (append 
+                                  ; generate contents of example-list according to show-examples
+                                  (map (lambda (e s) 
+                                         ; e is either empty or a two-element list of input and output
+                                         ; s is either a boolean or a three-element list of booleans
+                                         ;   indicating whether to show the funname/input/output
+                                         (dr-example funname e
+                                                     #:show-funname? (show-example-component? s 'funname)
+                                                     #:show-input? (show-example-component? s 'input)
+                                                     #:show-output? (show-example-component? s 'output))) 
+                                       example-list
+                                       ; pad show-examples to match length of example-list
+                                       ; padding value of #f means default is to not show example
+                                       (append show-examples
+                                               (build-list (- (length example-list) (length show-examples))
+                                                           (lambda (i) #f))))
+                                  ; pad example list with empty examples to get to specified num-examples
+                                  (build-list num-blank-examples (lambda (i) (dr-example funname '()))))
+                                 )])
+                          ; insert clear-breaks between examples and merge the example content lists
+                          (foldr (lambda (e res-rest) (append (cons (make-clear) e) res-rest))
+                                 '() example-elts))
+                        )
+                      (design-recipe-section 
+                       "recipe_definition"
+                       "Definition"
+                       "Write the definition, giving variable names to all your input values..."
+                       (make-spacer "(define ")
+                       (make-spacer "(")
+                       (make-wrapper
+                        (dr-student-answer #:id? #f "recipe_name" #:show? show-funname-defn? funname)
+                        (dr-student-answer "recipe_variables" #:show? (cons? show-params) 
+                                           (string-join param-list " "))
+                        (make-spacer ")")
+                        ;(make-clear)  ; only force this for long-form DR (maybe via a flag?)
+                        (dr-student-answer "recipe_definition_body" #:show? show-body? body)
+                        (make-spacer ")")))
                       )))))))
 
 (define (design-recipe-section id title instructions . body)
@@ -1498,11 +1553,11 @@
           (interleave-parbreaks/all
            (append (list (para #:style (bootstrap-div-style "sectionTitle") title)
                          (para #:style (bootstrap-div-style "sectionInstructions") instructions))
-                   body))))      
+                   (interleave-parbreaks/all body)))))     
 
 (define (make-wrapper . contents)
   (nested #:style (bootstrap-div-style "wrapper")
-          (interleave-parbreaks/all contents)))
+          (interleave-parbreaks/all contents))) 
 
 (define (make-spacer contents)
   (para #:style (bootstrap-span-style "spacer") contents))
@@ -1510,14 +1565,36 @@
 (define (make-clear)
   (para #:style (bootstrap-span-style "clear") ""))
   
+;; generate an example within a design recipe activity
+;; in-out-list is either empty or a list with the input and output expressions
+;;   these expressions can be any type, but will be formatted to strings
+;; the optional show arguments control which parts of the example are displayed
+(define (dr-example funname in-out-list
+                    #:show-funname? (show-funname? #f) 
+                    #:show-input? (show-input? #f)
+                    #:show-output? (show-output? #f)
+                    )
+  (let ([input (if (empty? in-out-list) "" (format "~a" (first in-out-list)))]
+        [output (if (empty? in-out-list) "" (format "~a" (second in-out-list)))])
+    (interleave-parbreaks/all
+     (list (make-spacer "(EXAMPLE ")
+           (make-spacer "(")
+           (make-wrapper
+            (dr-student-answer #:id? #f "recipe_name" #:show? show-funname? funname)
+            (dr-student-answer #:id? #f "recipe_example_inputs" #:show? show-input? input)
+            (make-spacer ")")
+            ;(make-clear) ; only force this for long-form DR (maybe via a flag?)
+            (dr-student-answer #:id? #f "recipe_example_body"#:show? show-output? output)
+            (make-spacer ")")))))) 
+
 (define (dr-student-answer class-or-id answer #:id? (id? #t) #:show? (show? #f))
   (let ([style (if id? 
                    (bootstrap-span-style/extra-id "studentAnswer" class-or-id) 
                    (bootstrap-span-style (string-append "studentAnswer " class-or-id)))])
     (para #:style style
-          (if (string? answer) 
-              (make-string (string-length answer) #\M)
-              " "))))
+          (cond [show? (format "~a" answer)]
+                [(string? answer) (make-string (string-length answer) #\M)]
+                [else  " "]))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
