c2d99c1b81b2ed129b923a61ef5219389bbb6954
Kathi Fisler <kfisler@cs.wpi.edu>
2014-03-29 08:17:27 -0400

    Created bs2 contentlist; hacked workbook-build to support bs2

--- build-workbook.rkt
+++ build-workbook.rkt
@@ -5,62 +5,29 @@
          racket/cmdline
          racket/path
          racket/file
-         "lib/translate-pdfs.rkt"
          "lib/paths.rkt"
          (lib "curr/lib/system-parameters.rkt")
          scribble/render
          file/zip)
 
-;;;;;;;;; STUFF FROM OTHER FILES
-       
-(define (make-fresh-document-namespace)
-  (define ns (make-base-namespace))
-  (for ([mod shared-modules])
-    (namespace-attach-module (namespace-anchor->namespace this-anchor) mod ns))
-  ns)
+;; NOTE: This defn is a hack.  Ideally, we should be using the
+;;  get-workbook-dir function from paths.rkt.  However, that is
+;;  capturing a binding for the current-course parameter that
+;;  I haven't figured out how to access when setting the current-course.
+;;  As a result, we always generate the bs1 workbook, even when our
+;;  current-course is set to bs2.
+(define (kf-get-workbook-dir) (build-path courses-base (current-course) "resources" "workbook"))
 
-(define-namespace-anchor this-anchor)
-(define shared-modules (list 'scribble/render
-                             '(lib "curr/lib/system-parameters.rkt")))
-
-(define document-namespace (make-fresh-document-namespace))
-
-(define (run-scribble scribble-file #:never-generate-pdf? [never-generate-pdf? #f]
-                                    #:include-base-path? [include-base-path? #f])
-  (define output-dir (cond [(current-deployment-dir)
-                            ;; Rendering to a particular deployment directory.
-                            (if include-base-path?
-                                (let-values ([(base name dir?)
-                                              (split-path
-                                               (find-relative-path (simple-form-path root-path)
-                                                                   (simple-form-path scribble-file)))])
-                                            (simple-form-path (build-path (current-deployment-dir) base)))
-                                (current-deployment-dir))]
-                           [else
-                            (error 'run-scribble "No deployment directory?")
-                            ;; In-place rendering
-                            #;(let-values ([(base name dir?)
-                                            (split-path (simple-form-path scribble-file))])
-                                base)]))
+(define (run-scribble scribble-file)
+  (define output-dir (current-deployment-dir))
   (define-values (base name dir?) (split-path scribble-file))
   (define output-path (build-path output-dir (string->path (regexp-replace #px"\\.scrbl$" (path->string name) ".html"))))
   (parameterize ([current-directory base]
-                 [current-namespace document-namespace]
                  [current-document-output-path output-path])
-      (printf "Current course is ~a~n" (current-course))
-
     (render (list (dynamic-require `(file ,(path->string name)) 'doc))
             (list name)
-            #:dest-dir output-dir)
-    (when (and (not never-generate-pdf?) (current-generate-pdf?))
-      (translate-html-to-pdf
-       (build-path output-dir
-                   (regexp-replace #px".scrbl$"
-                                   (path->string name)
-                                   ".html")))))
+            #:dest-dir output-dir))
   (void))
-
-;;;;;;;;;;;;;;
 
 ; converts list of pages to having all pdf extensions
 (define (pdf-pagenames pages)
@@ -85,8 +52,8 @@
         (with-output-to-file #:exists 'replace indexfile (lambda () (pretty-print (reverse indexlist))))
         (let ([p (first pages)])
           (let* ([output (with-output-to-string 
-                         (lambda () (system (format "pdftk ~a dump_data" 
-                                                    (path->string (build-path pdfdir p))))))]
+                          (lambda () (system (format "pdftk ~a dump_data" 
+                                                     (path->string (build-path pdfdir p))))))]
                  [match (regexp-match #px".*NumberOfPages: ([0-9]*).*" output)]
                  [numpages (if match (string->number (second match)) 
                                (error 'gen-wkbk-index 
@@ -105,33 +72,29 @@
 
 ;; the MAIN function to build the workbook
 (define (build-workbook)
-  (let ([pages (with-input-from-file (build-path (get-workbook-dir) "contentlist.rkt") read)]
-        [pagesdir (build-path (get-workbook-dir) "pages")])
-    (parameterize ([current-deployment-dir pagesdir])
-      (printf "Current course is ~a~n" (current-course))
-      (printf "Wkbk dir is ~a~n" (get-workbook-dir))
-      ; scribble the scrbl pages and run wkhtmltopdf on the resulting html
-      (for-each (lambda (f) 
-                  (when (regexp-match #px".*\\.scrbl$" f)
-                    (run-scribble (build-path pagesdir f))
-                     (let ([fhtml (regexp-replace #px"\\.scrbl$" f ".html")]
-                          [fpdf (regexp-replace #px"\\.scrbl$" f ".pdf")])
-                      ; -q option is for "quiet" operation
-                      (system (format "wkhtmltopdf -q ~a ~a" 
-                                      (path->string (build-path pagesdir fhtml))
-                                      (path->string (build-path pagesdir fpdf)))))))
-                pages)
-      )
-    (let* ([pdfpagenames (pdf-pagenames pages)])
-      (gen-wkbk-index pdfpagenames)
-      (merge-pages pdfpagenames)
-      ; add page numbers to final PD
-      )))
-
-
+  (let ([pages (with-input-from-file (build-path (kf-get-workbook-dir) "contentlist.rkt") read)]
+        [pagesdir (build-path (kf-get-workbook-dir) "pages")])
+    (if (empty? pages)
+        (printf "WARNING: Empty workbook contents for ~a, no workbook generated~n" (current-course))
+        (parameterize ([current-deployment-dir pagesdir])
+          ; scribble the scrbl pages and run wkhtmltopdf on the resulting html
+          (for-each (lambda (f) 
+                      (when (regexp-match #px".*\\.scrbl$" f)
+                        (run-scribble (build-path pagesdir f))
+                        (let ([fhtml (regexp-replace #px"\\.scrbl$" f ".html")]
+                              [fpdf (regexp-replace #px"\\.scrbl$" f ".pdf")])
+                          ; -q option is for "quiet" operation
+                          (system (format "wkhtmltopdf -q ~a ~a" 
+                                          (path->string (build-path pagesdir fhtml))
+                                          (path->string (build-path pagesdir fpdf)))))))
+                    pages)
+          (let* ([pdfpagenames (pdf-pagenames pages)])
+            (gen-wkbk-index pdfpagenames)
+            (merge-pages pdfpagenames)
+            ; add page numbers to final PD
+            )))))
 
 (define bootstrap-courses '("bs1" "bs2"))
 (for ([course (in-list bootstrap-courses)])
   (parameterize ([current-course course])
-    (printf "Starting to generate ~a ~n" course)
-    (build-workbook))) 
+    (build-workbook)))
---
+++ courses/bs2/resources/workbook/contentlist.rkt
@@ -0,0 +1,12 @@
+; This file specifies the pages that make up the workbook for the
+; course containing this resources/workbook directory.  ]
+
+; The list should contain names of either .scrbl or .pdf files that 
+; are in the workbook/pages directory.  Each list element must 
+; include either a .scrbl or a .pdf extension.
+
+; The workbook will be generated from these pages in order
+
+(
+
+ )
