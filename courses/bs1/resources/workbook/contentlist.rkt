704dd52cd6989a29ea9ad9e4d0038b10372f9a49
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2014-03-30 11:15:43 -0700

    update contentlist to use all the pdf pages from the workbook

--- courses/bs1/resources/workbook/contentlist.rkt
+++ courses/bs1/resources/workbook/contentlist.rkt
@@ -8,7 +8,53 @@
 ; The workbook will be generated from these pages in order
 
 (
- "take5.scrbl" 
- "double.scrbl"
  "Contracts.pdf"
+ ;;;;; UNIT 1 ;;;;;;;;;
+ "NinjaCat-Dissection.pdf"
+ "NinjaCat-Coordinates.pdf"
+ "Game-Design.pdf"
+ "Circle-of-Evaluation-Practice.pdf"
+ ;;;;; UNIT 2 ;;;;;;;;;
+ "Unit-2.pdf"
+ "Circles-of-Evaluation-Practice-2.pdf"
+ ;;;;; UNIT 3 ;;;;;;;;;
+ "Unit-3.pdf"
+ "Fast-Functions.pdf"
+ "Fast-Functions.pdf"
+ ;;;;; UNIT 4 ;;;;;;;;;
+ "Unit-4.pdf"
+ "rocket-height.pdf"
+ "red-square.pdf"
+ "yard-area.pdf"
+ ;;;;; UNIT 5 ;;;;;;;;;
+ "Unit-5.pdf"
+ "update-danger.pdf"
+ "update-target.pdf"
+ ;;;; UNIT 6 ;;;;;;;;;
+ "Unit-6.pdf"
+ "safe-left?.pdf"
+ "safe-right?.pdf"
+ "AND-and-OR.pdf"
+ "onscreen?.pdf"
+ ;;;; UNIT 7 ;;;;;;;;;
+ "Unit-7.pdf"
+ "cost.pdf"
+ "update-player.pdf"
+ ;;;; UNIT 8 ;;;;;;;;;
+ "Unit-8.pdf"
+ "line-length.pdf"
+ "distance-example.pdf"
+ "distance.pdf"
+ "collide?.pdf"
+ ;;;; UNIT 9 ;;;;;;;;;
+ "Unit-9.pdf"
+ "Lines.pdf"
+ "Feedback.pdf"
+ "Feedback.pdf"
+ "red-shape.pdf"
+ "Translating-to-Algebra.pdf"
+ "Math-Rocket-Distance.pdf"
+ "Math-Rocket-Time.pdf"
+ "Math-Rocket-Collision.pdf"
+ "Math-Blank-Recipe.pdf"
  )
