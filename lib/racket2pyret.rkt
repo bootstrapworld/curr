1d89e3804403939e4f6d33e71922458d53069e64
Kathi Fisler <kfisler@cs.wpi.edu>
2014-04-27 06:49:33 -0400

    created pyret ast and tostring

--- lib/racket2pyret.rkt
+++ lib/racket2pyret.rkt
@@ -2,6 +2,18 @@
 
 ; BS1 -> pyret compiler
 
+(provide bs1->pyret)
+
+;--------- PYRET AST -----------
+(define-struct pyatom (a))
+(define-struct pybinop (b l r))
+(define-struct pyfun (name args body))
+(define-struct pyask-clause (test then))
+(define-struct pyask (clauses))
+(define-struct pyexample (in out))
+(define-struct pyapp (fname args))
+
+;--------- BINOPS --------------
 (define binop-table
   '((+ plus)
     (- minus)
@@ -14,32 +26,54 @@
 (define (atom? v) (not (list? v)))
 
 (define (bs1->pyret sexp)
-  (if (atom? sexp) (format "~s" sexp)
-      (case (first sexp)
-        [(+ - * /) (format "~a(~a, ~a)" (lookup-binop (first sexp)) (second sexp) (third sexp))]
-        [(define) (let ([defname (first (second sexp))]
-                        [args (second (second sexp))]
-                        [body (third sexp)])
-                    (format "fun ~a(~a): ~n ~a ~nend" defname args (bs1->pyret body)))]
-        [(cond) (let* ([clauses (rest sexp)]
-                       [pyret-clauses 
-                        (map (lambda (c) (format " | ~a => ~a"
-                                                 (bs1->pyret (first c)) (bs1->pyret (second c))))
-                             clauses)])
-                  (format "ask: ~n ~a ~nend" (string-join pyret-clauses "~n ")))]
-        [(example) (let ([testexpr (second sexp)]
-                         [output (third sexp)])
-                     (format "~a is ~a" (bs1->pyret testexpr) (bs1->pyret output)))]
-        [else ;; function application
-         (let ([fname (first sexp)]
-               [args (rest sexp)])
-           (format "~a(~a)" fname (string-join (map bs1->pyret args) ", ")))]
-        )))
-        
+  (cond [(atom? sexp) (make-pyatom sexp)]
+        [else 
+         (case (first sexp)
+           [(+ - * /) (make-pybinop (first sexp) (bs1->pyret (second sexp)) (bs1->pyret (third sexp)))]
+           [(define) (let ([defname (first (second sexp))]
+                           [args (second (second sexp))]
+                           [body (third sexp)])
+                       (make-pyfun defname args (bs1->pyret body)))]
+           [(cond) (let* ([clauses (rest sexp)]
+                          [pyret-clauses 
+                           (map (lambda (c) (make-pyask-clause (bs1->pyret (first c)) (bs1->pyret (second c))))
+                                clauses)])
+                     (make-pyask pyret-clauses))]
+           [(example) (let ([testexpr (second sexp)]
+                            [output (third sexp)])
+                        (make-pyexample (bs1->pyret testexpr) (bs1->pyret output)))]
+           [else ;; function application
+            (let ([fname (first sexp)]
+                  [args (rest sexp)])
+              (make-pyapp fname (map bs1->pyret args)))]
+           )]))
+
+(define (pyret->string pyast)
+  (cond [(pyatom? pyast) (let ([a (pyatom-a pyast)])
+                           (if (string? a) a (format "~a" a)))]
+        [(pybinop? pyast) (format "~a(~a, ~a)" 
+                                  (lookup-binop (pybinop-b pyast)) 
+                                  (pyret->string (pybinop-l pyast)) 
+                                  (pyret->string (pybinop-r pyast)))]
+        [(pyfun? pyast) (format "fun ~a(~a): ~n ~a ~nend" 
+                                (pyfun-name pyast) 
+                                (pyfun-args pyast)
+                                (pyret->string (pyfun-body pyast)))]
+        [(pyask-clause? pyast) (format " | ~a => ~a"
+                                       (pyret->string (pyask-clause-test pyast))
+                                       (pyret->string (pyask-clause-then pyast)))]
+        [(pyask? pyast) (format "ask: ~n ~a ~nend" (string-join (map pyret->string (pyask-clauses pyast)) "~n "))]
+        [(pyexample? pyast) (format "~a is ~a"
+                                    (pyret->string (pyexample-in pyast))
+                                    (pyret->string (pyexample-out pyast)))]
+        [(pyapp? pyast) (format "~a(~a)" 
+                                (pyapp-fname pyast)
+                                (string-join (map pyret->string (pyapp-args pyast)) ", "))]
+        [else (error 'pyret->string "Unknown ast ~a~n" pyast)]))
         
 ;; TESTS
 (define (test-bs1->pyret expr)
-  (printf (bs1->pyret expr))
+  (display (pyret->string (bs1->pyret expr)))
   (printf "~n~n"))
 
 (test-bs1->pyret 3)
