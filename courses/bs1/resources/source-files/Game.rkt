4d6f2c45c145631745c79c477ebdf7af878680b5
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2014-06-09 06:48:35 -0400

    edit old racket file for Game template, so that EXAMPLEs are declared *after* the function

--- courses/bs1/resources/source-files/Game.rkt
+++ courses/bs1/resources/source-files/Game.rkt
@@ -30,19 +30,19 @@
 
 ; update-danger: Number -> Number
 ; Given the danger's OLD x-coordinate, output the NEXT x
-
-; Write your EXAMPLEs below this line:
-
 (define (update-danger x) 
   x)
+; Write your EXAMPLEs below this line:
+
+
 
 ; update-target : Number -> Number
 ; Given the target's OLD x-coordinate, output the NEXT x
 
-; Write EXAMPLEs here:
-
 (define (update-target x)
   x)
+; Write EXAMPLEs here:
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 2. Making the Danger and the Target Come Back Again: 
@@ -51,25 +51,25 @@
 ; safe-left? : Number -> Boolean
 ; Is the character protected on the left side of the screen?
 
-; Write an EXAMPLE that makes this true, and one that makes this false:
-
 (define (safe-left? x)
   true)
+; Write an EXAMPLE that makes this true, and one that makes this false:
+
 
 ; safe-right? : Number -> Boolean
 ; Is the character protected on the right side of the screen?
 
-; Write an EXAMPLE that makes this true, and one that makes this false:
-
 (define (safe-right? x)
   true)
+; Write an EXAMPLE that makes this true, and one that makes this false:
+
 
 ; onscreen? : Number -> Boolean
 ; Determines if the coordinates are within 100 pixels of the screen
-;; EXAMPLEs
-
 (define (onscreen? x)
   true)
+;; EXAMPLEs
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -80,7 +80,6 @@
 ; output the NEXT y.  The arrow keys are called "up" and "down".
 (define (update-player y key)
   y)
-
 ; EXAMPLE that makes it go up, one for down, and one to stay the same.
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -97,13 +96,12 @@
 
 ; line-length : Number Number -> Number
 ; the distance between two points on a number line
-(EXAMPLE (line-length 20 10) 10)
-(EXAMPLE (line-length 10 20) 10)
 (define (line-length a b)
   (cond
     [(> a b) (- a b)]
     [else (- b a)]))
-
+(EXAMPLE (line-length 20 10) 10)
+(EXAMPLE (line-length 10 20) 10)
   
 
 ; distance : Number Number Number Number -> Number
@@ -112,6 +110,8 @@
 ; HINT:   You can multiply a number x by itself using (sq x).
 ; HINT:   You can get the square root of a number x like (sqrt x).
 
+(define (distance px py cx cy)
+  0)
 ;(EXAMPLE (distance 20 20 20 20) 0)     ;; same point: distance is zero.
 ;(EXAMPLE (distance  1  0  0  0) 1)     ;; x difference of -1 only.
 ;(EXAMPLE (distance  0  2  0  0) 2)     ;; y difference of -2 only.
@@ -120,17 +120,14 @@
 ;(EXAMPLE (distance  0  0  3  4) 5)     ;; special right triangle at zero.
 ;(EXAMPLE (distance 15 22 10 10) 13)    ;; special right triangle.
 
-(define (distance px py cx cy)
-  0)
-
 
 
 ; collide? : Number Number Number Number -> Boolean 
 ; We have (px, py) and (cx, cy). Are they close enough for a collision?
-; EXAMPLE
-
 (define (collide? px py cx cy)
   false)
+; EXAMPLEs
+
 
 
 ; A final secret:
