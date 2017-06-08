5ac9c9ef74a720448cec0117209480831eb4bb57
jjackson12 <jacob_jackson@brown.edu>
2017-06-08 15:25:38 -0700

    updated translated file from form-elements

--- translating/translated.rkt
+++ translating/translated.rkt
@@ -12,10 +12,21 @@
 (define glossary
   '(
 
-    
+
+
+
+
+    ;;;;;;;;;;;;;;;;miscellanious;;;;;;;;;;;;;;;;;
+
+    ;Translator; this is a label to a button that flips things. Should be brief command
+    ;                TODO: UNSURE OF LOCATION               ;
+    ;Developer; insert-student-buttons
+    (btn-flip "flip")
+
+
     ;;;;;;;;;;;;;;;;copyright section;;;;;;;;;;;;;;;;;;;;;
     ;Translator; Found at the bottom of most pages; includes all copyright information
-    
+
     ;Translator; Names listed at bottom of most pages, included in the copyright statement
     ;Developler; copyright
     (copyright-names "Bootstrap by Emmanuel Schanzer, Emma Youndtsmith, Kathi Fisler, Shriram Krishnamurthi, Joe Politz and Ben Lerner is licensed under a ")
@@ -32,7 +43,34 @@
     (copyright-permissions "Permissions beyond the scope of this license may be available by contacting")
 
 
-    ;;;;;;;;;;;;;;;Bootstrap Language Table;;;;;;;;;;;;;;
+
+                ;;;Title;;;
+    ;Translator; This is the word that goes at the very top of the page, indicating what unit it is. IT will
+               ; followed by the number of what unit it is. Example: "Unit 3"
+    ;Developer; unit-summary-links
+    (unit "Unit")
+
+
+
+    ;;;;;;;;;;;;;;;Button Box;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;Translator; This is the box that floats on the top right corner of the page, with the
+               ; buttons "[Show/Hide] Teacher Notes", "Discussion Group", and "Slides".
+    ;Developer; insert-teacher-toggle-button
+
+    ;Translator;
+    (btn-show "Show Teacher Notes")
+    (btn-group "Discussion Group")
+    (btn-slide "Slides")
+
+
+
+
+    ;;;;;;;;;;;;;;;Intro Section;;;;;;;;;;;;;;
+    ;Translator; This is the gray box at the beginning of each unit page that contains overview information
+    ;Developer; agenda
+
+
+             ;;;Bootstrap Language Table;;;
     ;Translator; Found at the bottom of the intro/overview section at the beginning of unit pages
 
     ;Translator; Headers of the Bootstrap Language Table
@@ -41,21 +79,32 @@
     (lang-table-func "Functions")
 
 
-    ;;;;;;;;;;;;;;;Intro Section;;;;;;;;;;;;;;
-    ;Translator; This is the gray box at the beginning of each unit page that contains overview information
-    ;Developer; agenda
+
+             ;;;Standards Subtitle
+    ;Translator; The subtitle/description found beneath Standards and Evidence Statements.
+               ; NOTE that these three phrases appear together in the order listed- they should
+               ; be translated to be read clearly in succession, together.
+    ;Developer; learn-evid-from-standards
+    (standards-stitle "Standards with prefix BS are specific to Bootstrap; others are from the Common Core. Mouse over each standard to see its corresponding evidence statements. Our")
+    ;Translator; This is the Standards Document hyperlink text found within
+               ; the subtitle of the Standards and Evidence Statements section
+    (standards-link "Standards Document")
+    (standards-rest "shows which units cover each standard")
+
+
+
 
              ;;;Agenda;;;
     ;Translator; This is the White Agenda box in the top right that provides a listing of what sections are in that unit
 
-    ;Translator; Title of Agenda box   
+    ;Translator; Title of Agenda box
     (agenda-title "Agenda")
     ;Translator; Note that in English this is the word "minutes" abbreviated to "min". This is what is listed next to the
                ; timing of each section. Example: "Section 1   10 min"
     (agenda-min "min")
 
 
-  
+
 
              ;;;Headers;;;
 
@@ -67,9 +116,9 @@
     ;Developer; lesson/studteach
     (iHeader-overview "Overview")
 
-    
-    
-    
+
+
+
     ;Translator; All of these are headers found within the intro/overview section at the
                ; beginning of unit pages
     ;Developer; all functions are found underneath "Section of Units"
@@ -78,15 +127,18 @@
     ;
     ; These are also used within lesson/studteach because they are the same phrases
     (iHeader-materials "Materials and Equipment")
+    ;Developer; lesson/studteach
     (iHeader-mat "Materials")
     (iHeader-learning "Learning Objectives")
     (iHeader-evidence "Evidence Statementes")
     (iHeader-product "Product Outcomes")
     (iHeader-exercises "Exercises")
     (iHeader-preparation "Preparation")
-    
+    ;Developer; learn-evid-from-standards
+    (iHeader-standards "Standards and Evidence Statements")
 
-    
+
+
 
 
     ;;;;;;;;;;;;;;;;;;;;;;Lesson Sections;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -95,8 +147,7 @@
                ;lesson sections. simply change the word for time or duration
     ;Developer; lesson/studteach
     (sHeader-duration "Time")
-    
 
-    
-    )
 
+
+    ))
