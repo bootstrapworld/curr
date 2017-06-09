91c7720f160d3331752caff2e705dac1a1db6f72
Kielan Donahue <kielan@Kielans-MacBook-Air.local>
2017-06-09 11:05:34 -0400

    translation minor errors

--- translating/translated.rkt
+++ translating/translated.rkt
@@ -127,7 +127,7 @@
                ; the total time of the unit lesson
     ;Developer; length-of-lesson
     (length "Length")
-    (minutes "Minutes")
+    (minutes "minutes")
 
     
              ;;;Bootstrap Language Table;;;
