0cf32cfa4726efa6e3c1ad4ba272cef5cc07d562
jjackson12 <jacob_jackson@brown.edu>
2017-06-09 15:07:21 -0700

    gibber test on main

---
+++ courses/algebra/main-gibber.rkt
@@ -0,0 +1,45 @@
+#lang curr/lib
+
+@main-contents{
+@(elem #:style bs-coursename-style "Bootstrap:Algebra")
+
+@boxed-text{@animated-gif{splash.png}gibber@(hyperlink "../../Standards.shtml" "gibber")gibber}
+
+@bold{@(hyperlink "http://www.BootstrapWorld.org/workshops" "gibber")}
+
+@; workbook link names and targets
+gibber@italic{gibber}gibber
+@(apply itemlist
+        (append
+         (for/list ([n (in-range 1 (add1 9))])
+           (unit-summary/links n))
+         (list 
+          @summary-item/unit-link["gibber" "units/Supplemental/index"]{
+          gibber}
+          )
+         (list
+          ;;???
+          @summary-item/no-link["Student Workbook"]{
+           @lulu-button[]
+           gibber@hyperlink["http://www.lulu.com/commerce/index.php?fBuyContent=14790241" "gibber"], 
+               gibber @hyperlink["resources/workbook/StudentWorkbook.pdf" "gibber"].  gibber
+           @hyperlink["https://docs.google.com/a/bootstrapworld.org/spreadsheet/viewform?formkey=dHpQc0VHV2dBU2tSN1ptMktBUmtoZWc6MQ#gid=0"]{gibber}. }
+           
+          @item{@italic{gibber} 
+           @hyperlink["mailto:contact@bootstrapworld.org" "gibber"] gibber}
+          )
+         )
+         #:style "BSUnitList"
+)
+
+gibber
+@itemlist[#:style "BSResourceList"
+        @item{@(hyperlink "resources/teachers/index.shtml" "gibber" ) gibber
+               ;;???
+          @summary-item/custom["Support Forums" @(list (hyperlink "https://groups.google.com/d/forum/bootstrap-announce" "gibber")
+                                                       (hyperlink "https://groups.google.com/d/forum/bootstrap-discuss" "gibber"))]{
+            gibber}
+]
+            
+@(copyright)
+}
