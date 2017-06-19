7b5921ee079df5f158f2775a291b6288a57c82a3
Samuel Dooman <samuel.dooman@gmail.com>
2017-06-18 22:23:15 -0700

    [SD] add workbook to repo as .docx, start wb compilation (broken)

---
+++ courses/data-science/resources/workbook/contentlist.rkt
@@ -0,0 +1,63 @@
+; This file specifies the pages that make up the numbered portion of the 
+; workbook for the course containing this resources/workbook directory.  
+
+; The list should contain names of either .scrbl or .pdf files that 
+; are in the workbook/pages directory.  Each list element must 
+; include either a .scrbl or a .pdf extension.
+
+; The workbook will be generated from these pages in order
+
+(
+ ;;;;; UNIT 1 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Expressions-Values-Errors")
+ ("DSAWorkbook.pdf" "Identifiers-Expressions")
+ ("DSAWorkbook.pdf" "Contracts-Functions")
+
+ ;;;;; UNIT 2 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Animals")
+ ("DSAWorkbook.pdf" "Presidents-Nutrition")
+
+ ;;;;; UNIT 3 ;;;;;;;;;
+ ("ColleenPages.pdf" "Unit-3")
+ ("DSAWorkbook.pdf" "Fast-Functions")
+ ("DSAWorkbook.pdf" "Fast-Functions2")
+ ;;;;; UNIT 4 ;;;;;;;;;
+ ("ColleenPages.pdf" "Unit-4")
+ "rocket-height.scrbl"
+ "lawn-area.scrbl"
+ "red-square.scrbl"
+ ;;;;; UNIT 5 ;;;;;;;;;
+ ("ColleenPages.pdf" "Unit-5")
+ "update-danger.scrbl"
+ "update-target.scrbl"
+ ;;;; UNIT 6 ;;;;;;;;;
+ ("ColleenPages.pdf" "Unit-6")
+ ("DSAWorkbook.pdf" "Protecting-Sam")
+ "safe-left.scrbl"
+ "safe-right.scrbl"
+ ("DSAWorkbook.pdf" "AND-and-OR")
+ "onscreen.scrbl"
+ ;;;; UNIT 7 ;;;;;;;;;
+ ("ColleenPages.pdf" "Unit-7")
+ "cost.scrbl"
+ "update-player.scrbl"
+ ;;;; UNIT 8 ;;;;;;;;;
+ ("ColleenPages.pdf" "Unit-8")
+ "line-length.scrbl"
+ ("DSAWorkbook.pdf" "distance-example")
+ "distance.scrbl"
+ "collide.scrbl"
+ ;;;; UNIT 9 ;;;;;;;;;
+ ("ColleenPages.pdf" "Unit-9")
+ ("DSAWorkbook.pdf" "Game-Summary")
+ ("DSAWorkbook.pdf" "Lines")
+ ("DSAWorkbook.pdf" "Feedback")
+ ("DSAWorkbook.pdf" "Feedback")
+ "red-shape.scrbl"
+ ("DSAWorkbook.pdf" "Translating-to-Algebra")
+ ("DSAWorkbook.pdf" "Math-Rocket-Distance")
+ ("DSAWorkbook.pdf" "Math-Rocket-Time")
+ ("DSAWorkbook.pdf" "Math-Rocket-Collision")
+ ("DSAWorkbook.pdf" "Math-Blank-Recipe")
+ ("DSAWorkbook.pdf" "Math-Blank-Recipe")
+ )
