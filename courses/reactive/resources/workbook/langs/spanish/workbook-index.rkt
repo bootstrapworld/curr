2824eab5e6a342948550eb01265f6728db632bdc
Jacob Jackson <jacob_jackson@brown.edu>
2017-06-22 12:19:45 -0400

    forcibly adding back-matter in algebra and workbook-index in reactive

---
+++ courses/reactive/resources/workbook/langs/spanish/workbook-index.rkt
@@ -0,0 +1,56 @@
+;; This file is generated automatically.  DO NOT EDIT IT MANUALLY!
+
+;;for BS2, it is not generated, but is still manual
+(
+  ("Review" 1)
+  ("Review-page2" 2)
+  ("Fast-Functions-double" 3)
+ ("Fast-Functions-blank1" 4) 
+ ("Fast-Functions-blank2" 5)
+  ("Bug-Hunting" 6)
+;;;;; UNIT 2 ;;;;;;;;;
+("Unit-2" 7)
+  ("double-radius" 8)
+  ("double-width" 9)
+  ("next-position" 10)
+  ("Cakes" 11)
+ ("taller-than" 12)
+  ("will-melt" 13)
+ ;;;;; UNIT 3 ;;;;;;;;;
+  ("Unit-3" 14)
+  ("sunset-ds-design" 15)
+  ; 16 is page two of the sunset-ds-design
+  ("draw-state-sunset" 17)
+  ("next-state-sunset" 18)
+  ("anim-data-blank1" 19)
+  ("anim-data-blank2" 21)
+  ("anim-data-blank3" 23)
+  ("anim-data-blank4" 25)
+  ;;;;; UNIT 4 ;;;;;;;;;
+  ("Unit-4" 27)
+  ("location" 28)
+; 29 is page 2 of location 
+  ("piecewise-bug-hunting" 30)
+  ("extend-sunset-color" 31)
+  ("draw-sun" 33)
+  ;;;;; UNIT 5 ;;;;;;;;;
+  ("Unit-5" 35)
+  ("anim-design-pet" 36)
+  ("pet-key-extension" 38)
+  ("pet-sad-extension" 40)
+  ;;;;; SUPPLEMENTAL/FEATURES ;;;;;;;
+  ("BuildOwn" 42)
+  ("build-own-design" 43)
+  ("Collision" 45)
+  ("distance-formula" 46)
+  ("distance" 47)
+  ("is-collision" 48)
+  ("blank-dr1" 49)
+  ("blank-dr2" 50)
+  ("anim-design1" 51)
+  ("anim-design2" 53)
+  ("anim-design3" 55)
+  ("anim-extend1" 57)
+  ("anim-extend2" 59)
+  ("contracts" 61)
+  ) 
