2d25a12845b8a558f361e55c822a562d3141b1ae
Kathi Fisler <kfisler@cs.wpi.edu>
2017-06-26 15:53:11 -0400

    kathi fixed workbook build issue -- typo in build-workbook

---
+++ courses/data-science/resources/workbook/workbook-index.rkt
@@ -0,0 +1,79 @@
+;; This file is generated automatically.  DO NOT EDIT IT MANUALLY!
+
+; For data science, this file is managed manually
+; check the page numbers here -- Kathi made them up to get things to compile (6/26/17)
+
+(("Expressions-Values-Errors" 1)
+ ("Identifiers-Expressions" 1 )
+ ("Contracts-Functions" 1 )
+ ("Animals" 1 )
+ ("Presidents-Nutrition" 1 )
+ ("Mean-Median-Mode" 1 )
+ ("Measuring-Center" 1 )
+ ("Questions-With-Charts" 1 )
+ ("Freq-Bar-Chart" 1 )
+ ("Chart-Practice" 1 )
+ ("More-Chart-Practice" 1 )
+ ("Histograms" 1 )
+ ("Histograms-Practice" 1 )
+ ("Scatter-Plot" 1 )
+ ("Grading-Predictors" 1 )
+ ("More-Predictors" 1 )
+ ("Select-Practice" 1 )
+ ("Select-Continued" 1 )
+ ("Order-Practice" 1 )
+ ("Rows-Questions" 1 )
+ ("Booleans-Comparison" 1 )
+ ("Sieve-Practice" 1 )
+ ("Extending-Tables" 1 )
+ ("Countries-Example" 1 )
+ ("Nutrition-Plan" 1 )
+ ("Presidents-Plan" 1 )
+ ("Rainfall-Plan" 1 )
+ ("Countries-Plan" 1 )
+ )
+
+#|
+ ("DSAWorkbook.pdf" "Identifiers-Expressions")
+ ("DSAWorkbook.pdf" "Contracts-Functions")
+
+ ;;;;; UNIT 2 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Animals")
+ ("DSAWorkbook.pdf" "Presidents-Nutrition")
+
+ ;;;;; UNIT 3 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Mean-Median-Mode")
+ ("DSAWorkbook.pdf" "Measuring-Center")
+ ;;;;; UNIT 4 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Questions-With-Charts")
+ ("DSAWorkbook.pdf" "Freq-Bar-Chart")
+ ("DSAWorkbook.pdf" "Chart-Practice")
+ ("DSAWorkbook.pdf" "More-Chart-Practice")
+
+ ;;;;; UNIT 5 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Histograms")
+ ("DSAWorkbook.pdf" "Histogram-Practice")
+
+ ;;;; UNIT 6 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Scatter-Plot")
+ ("DSAWorkbook.pdf" "Grading-Predictors")
+ ("DSAWorkbook.pdf" "More-Predictors")
+ 
+ ;;;; UNIT 7 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Select-Practice")
+ ("DSAWorkbook.pdf" "Select-Continued")
+ ("DSAWorkbook.pdf" "Order-Practice")
+
+ ;;;; UNIT 8 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Rows-Questions")
+ ("DSAWorkbook.pdf" "Booleans-Comparison")
+ ("DSAWorkbook.pdf" "Sieve-Practice")
+
+ ;;;; UNIT 9 ;;;;;;;;;
+ ("DSAWorkbook.pdf" "Extending-Tables")
+ ("DSAWorkbook.pdf" "Countries-Example")
+ ("DSAWorkbook.pdf" "Nutrition-Plan")
+ ("DSAWorkbook.pdf" "Presidents-Plan")
+ ("DSAWorkbook.pdf" "Rainfall-Plan")
+ ("DSAWorkbook.pdf" "Countries-Plan")
+|#
