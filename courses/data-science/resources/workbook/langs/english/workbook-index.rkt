c55c217c80197c5da827cfedcb2b77c760e3f9f4
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2017-10-28 06:13:59 -0600

    [DS] add blank DR and TP pages to workbook

--- courses/data-science/resources/workbook/langs/english/workbook-index.rkt
+++ courses/data-science/resources/workbook/langs/english/workbook-index.rkt
@@ -4,14 +4,14 @@
 ; check the page numbers here -- Kathi made them up to get things to compile (6/26/17)
 
 (
-;;;;;;;;;;;;;;;; Unit 1;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; Unit 1 ;;;;;;;;;;;;;
  ("Unit-1-Notes" 	 			 1 )
  ("Unit-1-Blank" 	 			 2 )
  ("Numbers-and-Strings" 	 	 3 )
  ("Booleans" 					 4 )
  ("Fill-in-Value" 			 	 5 )
 
-;;;;;;;;;;;;;;;; Unit 2;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; Unit 2 ;;;;;;;;;;;;;
  ("Unit-2-Notes" 	 	 		 6 )
  ("Unit-2-Blank" 	 	 		 7 )
  ("Animals-Dataset" 			 8 )
@@ -21,7 +21,7 @@
  ("My-Dataset" 					12 )
  ("Blank"						13 )
 
-;;;;;;;;;;;;;;;; Unit 3;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; Unit 3 ;;;;;;;;;;;;;
  ("Unit-3-Notes" 	 			14 )
  ("Unit-3-Blanks" 	 			15 )
  ("Reviewing-Functions" 		16 )
@@ -32,7 +32,7 @@
  ("Fixed-Birth" 				21 )
  ("Dataset-Plans" 				22 )
 
-;;;;;;;;;;;;;;;; Unit 4;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; Unit 4 ;;;;;;;;;;;;;
  ("Unit-4-Notes"				23 )
  ("Unit-4-Blank"				24 )
  ("Measuring-Center" 			25 )
@@ -40,7 +40,7 @@
  ("Mean-Kitten-Adoption" 		27 )
  ("Measuring-Center-in-Dataset" 28 )
 
-;;;;;;;;;;;;;;;; Unit 5;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; Unit 5 ;;;;;;;;;;;;;
  ("Unit-5-Notes" 				29 )
  ("Unit-5-Blank" 				30 )
  ("Column-Statements" 			31 )
@@ -52,7 +52,7 @@
  ("Blank-Table-Plan-2"			37 )
  ("Quantity-My-Dataset"			38 )
 
-;;;;;;;;;;;;;;;; Unit 6;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; Unit 6 ;;;;;;;;;;;;;
  ("Unit-6-Notes" 	 			39 )
  ("Unit-6-Blank" 	 			40 )
  ("Visualizing-Quantity-Review" 41 )
@@ -62,7 +62,7 @@
  ("Frequency-My-Dataset"		45 )
  ("Blank"						46 )
 
-;;;;;;;;;;;;;;;; Unit 7;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; Unit 7 ;;;;;;;;;;;;;
  ("Unit-7-Notes" 	 			47 )
  ("Unit-7-Blank" 	 			48 )
  ("Cuter-Adoption" 	 			49 )
@@ -72,12 +72,20 @@
  ("Scatter-Dogs-Age" 			53 )
  ("Correlations-My-Dataset" 	54 )
 
-;;;;;;;;;;;;;;;; Unit 8;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; Unit 8 ;;;;;;;;;;;;;
  ("Unit-8-Notes" 	 			55 )
  ("Unit-8-Blank" 	 			56 )
 
-;;;;;;;;;;;;;;;; Unit 9;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;; Unit 9 ;;;;;;;;;;;;;
  ("Unit-9" 	 					57 )
- ("Contracts-1" 				58 )
- ("Contracts-2" 				59 )
+
+;;;;;;;;;;;;;;; The End ;;;;;;;;;;;;;
+ ("Blank-Design-Recipes-1" 	 	59 )
+ ("Blank-Design-Recipes-2" 	 	60 )
+ ("Blank-Design-Recipes-3" 	 	61 )
+ ("Blank-Table-Plan-3" 		 	62 )
+ ("Blank-Table-Plan-4" 		 	63 )
+ ("Blank-Table-Plan-5" 		 	64 )
+ ("Contracts-1" 				65 )
+ ("Contracts-2" 				66 )
  )
