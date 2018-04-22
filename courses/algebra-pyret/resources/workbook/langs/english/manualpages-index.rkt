57b0a6e3c8f7482b2a781f9c77add1e6ddde4dd7
Daniel Oshiro <doshiro@ucsd.edu>
2018-04-22 15:12:19 -0700

    Updated page number and strings to match current DSA-Workbook.pdf numbering

--- courses/algebra-pyret/resources/workbook/langs/english/manualpages-index.rkt
+++ courses/algebra-pyret/resources/workbook/langs/english/manualpages-index.rkt
@@ -64,83 +64,9 @@
  ("Math-Rocket-Collision" 41)
  ("Math-Blank-Recipe" 42)
 ;;;;;;;;;;;;;;;; Unit 201 ;;;;;;;;;;;;;
- ("Unit-1-Notes" 	 			 1 )
- ("Unit-1-Blank" 	 			 2 )
- ("Numbers-and-Strings" 	 	 3 )
- ("Booleans" 		4					 4 )
-
-;;;;;;;;;;;;;;;; Unit 202 ;;;;;;;;;;;;;
- ("Unit-2-Notes" 	 	 		 5 )
- ("Unit-2-Blank" 	 	 		 6 )
- ("Animals-Dataset" 			 7 )
- ("Design-Recipe-1" 			 8 )
- ("Design-Recipe-2" 			 9 )
- ("Design-Recipe-3" 			10 )
- ("My-Dataset" 					11 )
- ("Blank" 						12 )
-;;;;;;;;;;;;;;;; Unit 203 ;;;;;;;;;;;;;
- ("Unit-3-Notes" 	 			13 )
- ("Unit-3-Blanks" 	 			14 )
- ("Reviewing-Functions" 		15 )
- ("Animals-Plans" 				16 )
- ("Methods" 					17 )
- ("Kitten-Tags"					18 )
- ("Dogs-by-Age" 				19 )
- ("Old-Dogs-Diet" 				20 )
- ("Fixed-Birth" 				21 )
- ("Dataset-Plans" 				22 )
-
-;;;;;;;;;;;;;;;; Unit 204 ;;;;;;;;;;;;;
- ("Unit-4-Notes"				23 )
- ("Unit-4-Blank"				24 )
- ("Column-Statements" 			25 )
- ("Visualizing-Quantity" 		26 )
- ("Pie-Dog-Weight" 				27 )
- ("Bad-Sample-Tables"			28 )
- ("Bar-Kitten-Adoption"			29 )
- ("Blank-Table-Plan-1"			30 )
- ("Blank-Table-Plan-2"			31 )
- ("Visualizing-My-Dataset-1"	32 )
- 
-;;;;;;;;;;;;;;;; Unit 205 ;;;;;;;;;;;;;
- ("Unit-5-Notes" 				33 )
- ("Unit-5-Blank" 				34 )
- ("Summarizing-Weeks" 			35 )
- ("Variation-Dog-Age" 			36 )
- ("Interpreting-Variation" 		37 )
- ("Summarizing-My-Dataset" 		38 )
-
-;;;;;;;;;;;;;;;; Unit 206 ;;;;;;;;;;;;;
- ("Unit-6-Notes" 	 			39 )
- ("Unit-6-Blank" 	 			40 )
- ("Visualizing-Quantity-Review" 41 )
- ("Visualizing-Frequency" 		42 )
- ("Freq-Bar-Gender" 			43 )
- ("Histogram-Adoption" 			44 )
- ("Visualizing-My-Dataset-2"	45 )
- ("Which-Chart-Is-Best"			46 )
-
-;;;;;;;;;;;;;;;; Unit 207 ;;;;;;;;;;;;;
- ("Unit-7-Notes" 	 			47 )
- ("Unit-7-Blank" 	 			48 )
- ("Cuter-Adoption" 	 			49 )
- ("Make-Scatter-Plot"  			50 )
- ("Age-v-Weeks-Dogs" 			51 )
- ("Drawing-Predictors" 			52 )
- ("Scatter-Dogs-Age" 			53 )
- ("Correlations-My-Dataset" 	53 )
- ("Blank-2" 					54 )
-
-;;;;;;;;;;;;;;;; Unit 208 ;;;;;;;;;;;;;
- ("Unit-8-Notes" 	 			55 )
- ("Unit-8-Blank" 	 			56 )
- ("Grading-Predictors" 			57 )
- ("Findings-Animals" 			58 )
- ("Findings-My-Dataset" 		59 )
- 
-;;;;;;;;;;;;;;;; Unit 209 ;;;;;;;;;;;;;
- ("Unit-9-Notes" 	 			60 )
- ("Fake-News" 		 			61 )
-
+ ("Unit-1-Notes" 	 			 3 )
+ ("Unit-1-Blank" 	 			 4 )
+ ("Numbers-and-Strings" 	 	 5 )
+ ("Booleans" 					 6 )
 
 )
