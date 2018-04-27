1cf471a207567cd3beadc38682a736af2239d88f
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2018-04-27 14:00:09 -0400

    make room for new Unit 2

--- courses/data-science/resources/workbook/langs/english/workbook-index.rkt
+++ courses/data-science/resources/workbook/langs/english/workbook-index.rkt
@@ -12,8 +12,10 @@
  ("Examples" 					 6 )
 
 ;;;;;;;;;;;;;;;; Unit 2 ;;;;;;;;;;;;;
- ("Unit-2-Notes" 	 	 		 7 )
- ("Unit-2-Blank" 	 	 		 8 )
+
+;;;;;;;;;;;;;;;; Unit 3 ;;;;;;;;;;;;;
+ ("Unit-3-Notes" 	 	 		 7 )
+ ("Unit-3-Blank" 	 	 		 8 )
  ("Animals-Dataset" 			 9 )
  ("Design-Recipe-1" 			10 )
  ("Design-Recipe-2" 			11 )
@@ -21,27 +23,27 @@
  ("My-Dataset" 					13 )
  ("Padding-Unit-2" 				14 )
 
-;;;;;;;;;;;;;;;; Unit 3 ;;;;;;;;;;;;;
- ("Unit-3-Notes" 	 			15 )
- ("Unit-3-Blank" 	 			16 )
+;;;;;;;;;;;;;;;; Unit 4 ;;;;;;;;;;;;;
+ ("Unit-4-Notes" 	 			15 )
+ ("Unit-4-Blank" 	 			16 )
  ("Reviewing-Functions" 		17 )
  ("Animals-Plans" 				18 )
  ("Methods" 					19 )
  ("Playing-With-Methods" 		20 )
 
-;;;;;;;;;;;;;;;; Unit 4 ;;;;;;;;;;;;;
- ("Unit-4-Notes" 	 			21 )
- ("Unit-4-Blank" 	 			22 )
- ("Unit-4-Review" 	 			23 )
+;;;;;;;;;;;;;;;; Unit 5 ;;;;;;;;;;;;;
+ ("Unit-5-Notes" 	 			21 )
+ ("Unit-5-Blank" 	 			22 )
+ ("Unit-5-Review" 	 			23 )
  ("Kitten-Tags"					24 )
  ("Dogs-by-Age" 				25 )
  ("Old-Dogs-Diet" 				26 )
  ("Fixed-Birth" 				27 )
  ("Dataset-Plans" 				28 )
 
-;;;;;;;;;;;;;;;; Unit 5 ;;;;;;;;;;;;;
- ("Unit-5-Notes"				29 )
- ("Unit-5-Blank"				30 )
+;;;;;;;;;;;;;;;; Unit 6 ;;;;;;;;;;;;;
+ ("Unit-6-Notes"				29 )
+ ("Unit-6-Blank"				30 )
  ("Column-Statements" 			31 )
  ("Visualizing-Quantity" 		32 )
  ("Pie-Dog-Weight" 				33 )
@@ -51,17 +53,17 @@
  ("Blank-Table-Plan-2"			37 )
  ("Visualizing-My-Dataset-1"	38 )
  
-;;;;;;;;;;;;;;;; Unit 6 ;;;;;;;;;;;;;
- ("Unit-5-Notes" 				39 )
- ("Unit-5-Blank" 				40 )
+;;;;;;;;;;;;;;;; Unit 7 ;;;;;;;;;;;;;
+ ("Unit-7-Notes" 				39 )
+ ("Unit-7-Blank" 				40 )
  ("Summarizing-Weeks" 			41 )
  ("Variation-Dog-Age" 			42 )
  ("Interpreting-Variation" 		43 )
  ("Summarizing-My-Dataset" 		44 )
 
-;;;;;;;;;;;;;;;; Unit 7 ;;;;;;;;;;;;;
- ("Unit-6-Notes" 	 			45 )
- ("Unit-6-Blank" 	 			46 )
+;;;;;;;;;;;;;;;; Unit 8 ;;;;;;;;;;;;;
+ ("Unit-8-Notes" 	 			45 )
+ ("Unit-8-Blank" 	 			46 )
  ("Visualizing-Quantity-Review" 47 )
  ("Visualizing-Frequency" 		48 )
  ("Freq-Bar-Gender" 			49 )
@@ -69,9 +71,9 @@
  ("Visualizing-My-Dataset-2"	51 )
  ("Which-Chart-Is-Best"			52 )
 
-;;;;;;;;;;;;;;;; Unit 8 ;;;;;;;;;;;;;
- ("Unit-7-Notes" 	 			53 )
- ("Unit-7-Blank" 	 			54 )
+;;;;;;;;;;;;;;;; Unit 9 ;;;;;;;;;;;;;
+ ("Unit-9-Notes" 	 			53 )
+ ("Unit-9-Blank" 	 			54 )
  ("Cuter-Adoption" 	 			55 )
  ("Make-Scatter-Plot"  			56 )
  ("Age-v-Weeks-Dogs" 			57 )
@@ -79,16 +81,16 @@
  ("Scatter-Dogs-Age" 			59 )
  ("Correlations-My-Dataset" 	60 )
  
-;;;;;;;;;;;;;;;; Unit 9 ;;;;;;;;;;;;;
- ("Unit-8-Notes" 	 			61 )
- ("Unit-8-Blank" 	 			62 )
+;;;;;;;;;;;;;;;; Unit 10 ;;;;;;;;;;;;;
+ ("Unit-10-Notes" 	 			61 )
+ ("Unit-10-Blank" 	 			62 )
  ("Grading-Predictors" 			63 )
  ("Findings-Animals" 			64 )
  ("Findings-My-Dataset" 		65 )
  ("Padding-Unit-8" 				66 )
  
-;;;;;;;;;;;;;;;; Unit 10 ;;;;;;;;;;;;
- ("Unit-9-Notes" 	 			67 )
+;;;;;;;;;;;;;;;; Unit 11 ;;;;;;;;;;;;
+ ("Unit-11-Notes" 	 			67 )
  ("Fake-News" 		 			68 )
 
 ;;;;;;;;;;;;;;; The End ;;;;;;;;;;;;;
