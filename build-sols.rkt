d12011ad4e7b62baa2edd4b95ef40241a9781d10
Kathi Fisler <kfisler@cs.brown.edu>
2018-05-05 14:04:02 -0400

    solution building fixed for spanish

--- build-sols.rkt
+++ build-sols.rkt
@@ -13,6 +13,7 @@
          racket/cmdline
          racket/path
          racket/file
+         racket/list
          (lib "curr/lib/system-parameters.rkt")
          "lib/translate-pdfs.rkt"
          "lib/paths.rkt"
@@ -42,6 +43,24 @@
 (define document-namespace (make-fresh-document-namespace))
 
 
+;;This lists all courses which are currently able to be built
+(define available-course-specs '(("algebra" "english" "spanish")
+                                 ("algebra-pyret" "english")
+                                 ("reactive" "english")
+                                 ("data-science" "english")
+                                 ("physics" "english")
+                                 ;("blank-course" "english")
+				 ))
+(define available-courses (map (lambda (course-spec) (first course-spec)) available-course-specs))
+
+(define available-languages (list "english" "spanish"))
+(define courses available-courses)
+
+;(define bootstrap-course-specs available-course-specs)
+
+(define run-languages (list "english" "spanish"))
+
+
 ;; run-scribble: path -> void
 ;; Runs scribble on the given file.
 (define (run-scribble scribble-file #:never-generate-pdf? [never-generate-pdf? #f])
@@ -57,6 +76,44 @@
     )  
   (void))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; parse-course-args: list/of string -> list/of string
+;; This parses the list of course arguments, ensuring that they are all valid course names
+(define (parse-course-args rest-args)
+  (cond
+    [(empty? rest-args) empty]
+    [(cons? rest-args)
+     ;;checks if next argument is a command-line argument tag, rather than a course name
+     (let [(course-name (first rest-args))]
+       (if (member course-name available-courses)
+           (cons course-name (parse-course-args (rest rest-args)))
+           (error (format (string-append "Build got unrecognized target course: " course-name "\n expected one of the following:\n~a\n")
+                  available-courses))))]))
+
+;; parse-unit-args: list/of string -> list/of string
+;; This parses the list of unit arguments
+(define (parse-unit-args rest-args)
+  (cond
+    [(empty? rest-args) empty]
+    [(cons? rest-args)
+     ;;checks if next argument is a command-line argument tag, rather than a Unit name
+     (let [(unit-name (first rest-args))]
+       (if (string-contains? unit-name "unit")
+           (cons unit-name (parse-unit-args (rest rest-args)))
+           (error (format (string-append "Build got unrecognized target Unit " unit-name "\n expected something that looks like Unitx")))))]))
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;; Warnings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (parse-lang-args args)
+  (filter (lambda (arg)
+            (unless (member arg available-languages)
+                (error "Build got unrecognized target language: " arg " -- expected english or spanish"))
+            (member arg available-languages))
+            args))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Command line parsing.  We initialize the SCRIBBLE_TAGS environmental
@@ -68,6 +125,10 @@
    #:once-each
    [("--deploy") -deploy-dir "Deploy into the given directory, and create a .zip.  Default: deploy" 
     (current-deployment-dir (simple-form-path -deploy-dir))]
+   [("--language") -language "Select what language you are printing the curriculum for. Default: english"
+                   (set! run-languages (parse-lang-args (string-split -language "_")))]
+   [("--course") -course "List all courses that you want to build. They MUST be separated by \"_\"_. Default: All available courses"
+                 (set! courses (parse-course-args (string-split -course "_")))]
    #:args tags
    tags))
 
@@ -83,9 +144,9 @@
     (unless (directory-exists? (build-path (deploy-resources-dir) "teachers" "solutions"))
       (make-directory (build-path (deploy-resources-dir) "teachers" "solutions")))
     (parameterize ([current-deployment-dir (build-path (deploy-resources-dir) "teachers" "solutions")])
-      (for ([subdir (directory-list lessons-dir)]
-            #:when (directory-exists? (build-path lessons-dir subdir)))
-        (let ([exercises-path (build-path lessons-dir subdir "exercises")])
+      (for ([subdir (directory-list (lessons-dir))]
+            #:when (directory-exists? (build-path (lessons-dir) subdir)))
+        (let ([exercises-path (build-path (lessons-dir) subdir "exercises")])
           (when (directory-exists? exercises-path)
             (for ([worksheet (directory-list exercises-path)]
                   #:when (regexp-match #px".scrbl$" worksheet))
@@ -99,8 +160,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main entry point:
+
 (putenv "TARGET-LANG" "racket")
 (putenv "RELEASE-STATUS" "mature")
+
+(define (update-lang-fields language)
+  (putenv "LANGUAGE" language)
+  (printf "\n\nbuild.rkt: building in language ~a~n" (getenv "LANGUAGE"))
+  (current-translations (with-input-from-file (string-append "lib/langs/" (getenv "LANGUAGE") "/translated.rkt") read))
+  (current-glossary-terms (with-input-from-file (string-append "lib/langs/" (getenv "LANGUAGE") "/glossary-terms.rkt") read)))
+
+(update-lang-fields "spanish")
+
 (build-exercise-handout-solutions)
 ;; need to create an index file for the solutions
 ;; ideally include solutions to the activities as well
