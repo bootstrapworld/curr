475ce92653ea116df8a8a18defa61b1bdd7b02b3
Joe Gibbs Politz <joe.politz@gmail.com>
2018-06-03 22:34:57 -0700

    A bunch of template hacking; index "works", course pages need more
    The main idea is to use racket web-server/templates with backtick (`) as the
    templating character. (@ is a pain because it is used for email addresses). All
    main/index HTML files will be passed through the templater, which can fill in
    the menubar (and other variables, if needed, in the future).
    
    A bunch of images needed are not in this repo, so I've made them refer to
    Bootstrapworld.org/images directly; they could be downloaded and copied into a
    suitable location within this repo.

--- build.rkt
+++ build.rkt
@@ -9,6 +9,7 @@
          racket/list
          racket/match
          racket/vector
+         web-server/templates
          (for-syntax racket/base)
          (planet neil/csv:1:=7)
          "lib/system-parameters.rkt"
@@ -313,14 +314,22 @@
           (make-directory (current-deployment-dir))])
 
   (copy-file overview-styles.css (build-path (current-deployment-dir) "styles.css"))
+
+  (when (not (directory-exists? (build-path (current-deployment-dir) "images")))
+    (make-directory (build-path (current-deployment-dir) "images")))
+
+  (copy-file "lib/logo.png" (build-path (current-deployment-dir) "images" "logo.png"))
+  (copy-file "lib/logo-icon.png" (build-path (current-deployment-dir) "images" "icon.png"))
   
   (for ([base (directory-list (static-pages-path))])
     (define source-full-path (build-path (static-pages-path) base))
     (define target-full-path (build-path (current-deployment-dir) base))
-    (when (or (file-exists? target-full-path)
-              (directory-exists? target-full-path))
-      (delete-directory/files target-full-path))
-    (copy-directory/files source-full-path target-full-path)))
+    (printf "Is ~a a directory? ~a\n" source-full-path (directory-exists? source-full-path))
+    (when (file-exists? source-full-path)
+      (copy-with-templates! (path->string source-full-path) target-full-path))
+    (when (directory-exists? target-full-path)
+      (delete-directory/files target-full-path)
+      (copy-directory/files source-full-path target-full-path))))
 
 #| 
 ;;;;;;;  DEAD CODE  ;;;;;;;;;;;;
@@ -330,6 +339,18 @@
   (printf "deployment path: ~s\n" (current-deployment-dir))
   (printf "-------\n"))
 |#
+
+
+; NOTE(joe): https://lists.racket-lang.org/users/archive/2013-June/058135.html
+(define menubar (file->string (build-path "lib/" "menubar.html")))
+(define (template-content template-path menubar)
+  (eval #`(let ([menubar #,menubar]) (include-template #:command-char #\` (file #,template-path)))))
+
+(define (copy-with-templates! source dest)
+  (define templated (template-content source menubar))
+  (define to-write (open-output-file dest))
+  (printf "Displaying to ~a\n" dest)
+  (display templated to-write))
 
 
 ;; Building the units of the course.
@@ -414,9 +435,17 @@
 
   ;; rename the course directory
   (printf "build.rkt: renaming directory for ~a \n" (current-course))
+  (copy-with-templates!
+    (path->string (build-path (current-deployment-dir) "courses" (current-course)(getenv "LANGUAGE") "index.html"))
+    (build-path (current-deployment-dir) "courses" (current-course)(getenv "LANGUAGE") "index.shtml"))
+
+
+#|
   (rename-file-or-directory (build-path (current-deployment-dir) "courses" (current-course)(getenv "LANGUAGE") "index.html")
                             (build-path (current-deployment-dir) "courses" (current-course)(getenv "LANGUAGE") "index.shtml")
                             #t)
+|#
+
   (unless (directory-exists? (get-units-dir)) 
      (WARNING (format "No units directory found for course ~a in language ~a" 
 		      (current-course) (getenv "LANGUAGE")) 'no-course-dir))
